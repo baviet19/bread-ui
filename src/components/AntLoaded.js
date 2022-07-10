cd79b27420a09f48b6f92c9b12a792e43e8e2978
DESKTOP-CB8N8DC\Viet <n17dccn145@student.ptithcm.edu.vn>
2022-07-11 05:33:18 +0700

    Revert "first commit"
    This reverts commit ce51a9f1ed477a8992232326126a8ee297a1c7ae.

--- src/components/AntLoaded.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Spin, message } from 'antd';
-
-const AntLoader = () => {
-  const key = 'updatable';
-  return message.success({ content: 'Loaded!', key, duration: 2 });
-};
-
-export default AntLoader;
--- src/helper/getFullDate.js
+++
@@ -1,5 +0,0 @@
-export const getFullDate = (date) => {
-  const dateAndTime = date.split('T');
-
-  return dateAndTime[0].split('-').reverse().join('-');
-};
